dc43ce40384d1f44ed60c0e53efbd0226d4f54cc
Prathmesh <prathmesh.bagul@klingelnberg.com>
2022-06-13 10:56:53 +0530

    AdditionOfElement

--- Practice/Class1.cs
+++ Practice/Class1.cs
@@ -10,24 +10,37 @@ namespace Practice
     {
         static void Main(string[] args) {
 
-            var x = 34.2;
-            Console.WriteLine(x);
-            var y = 27.8;
-            Console.WriteLine(y);
-            var result = x + y;
-            Console.WriteLine("x" +"+"+ "y");
-            Console.WriteLine(result );   
 
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]} !");
-            }
-            else
-            {
-                Console.WriteLine("Hello !");
-            }
-        
-        
+            var numbers = new double[3];
+            numbers[0]=17.6;
+            numbers[1] = 12.4;
+            numbers[2] = 10.0;
+
+            var result = numbers[0];
+            result = result + numbers[1];
+            result = result + numbers[2];   
+            Console.WriteLine(result);
+
+
+
+            /* var x = 34.2;
+             Console.WriteLine(x);
+             var y = 27.8;
+             Console.WriteLine(y);
+             var result = x + y;
+             Console.WriteLine("x" +"+"+ "y");
+             Console.WriteLine(result );   
+
+             if (args.Length > 0)
+             {
+                 Console.WriteLine($"Hello, {args[0]} !");
+             }
+             else
+             {
+                 Console.WriteLine("Hello !");
+             }
+         */
+
         }
     }
 }
