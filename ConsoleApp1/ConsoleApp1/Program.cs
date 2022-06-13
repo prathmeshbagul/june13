bb05fee0ce698eed2bcc3151b0eb0e7c589d2adc
Prathmesh <prathmesh.bagul@klingelnberg.com>
2022-06-13 16:08:50 +0530

    EvenOrOdd

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Coding
+{
+    class Program
+    {
+
+       public static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Enter number");
+             a = Convert.ToInt32 (Console.ReadLine());
+
+            FindEvenOrOdd( a);
+
+
+
+        }
+
+        public static void FindEvenOrOdd(int a) {
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Even");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+                Console.Read();
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
